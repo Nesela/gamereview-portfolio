0fa690c47586b4b4225c1ee698af1a8f49aa3072
Nesela <icarus_cat@naver.com>
2025-12-18 18:54:50 +0900

    Add files via upload
    게시글 리스트부과 글쓰기를 변수로 따로 지정
    정규 표현식을 사용

--- js/board.js
+++ js/board.js
@@ -3,8 +3,6 @@ let editor;
 const board_post = document.querySelector("#new_post")
 const myGameListElement = document.querySelector("#game-list");
 
-const mainBoard = document.querySelector(".board")
-
 const createEditor = () => {
     editor = new toastui.Editor({
         el: document.querySelector('#editor'),
@@ -15,34 +13,90 @@ const createEditor = () => {
     });
 }
 
-document.querySelectorAll(".board a").forEach(board => {
-    board.addEventListener("click", () => {
-        myGameListElement.className = "post_list";
-        myGameListElement.innerHTML = "";
+const getUserPost = () => {
+    const data = localStorage.getItem("userPost");
+    return data ? JSON.parse(data) : [];
+}
 
-        const m_board = board.innerText;
-        board_post.innerHTML = m_board;
-
-        mygameList.innerHTML += `
+// 게시글 리스트
+const showboardList = (category) => {
+    myGameListElement.className = "post_list";
+    myGameListElement.innerHTML = "";
+    board_post.innerHTML = category;
+    mygameList.innerHTML += `
         <div>
-        <button id="board_bt">글쓰기</button>
+        <button id="topBoardBt" class="board_bt">글쓰기</button>
+        <div></div>
+        <button id="bottomBoardBt" class="board_bt">글쓰기</button>
         </div>
         `
-        const board_bt = document.querySelector("#board_bt");
-        board_bt.addEventListener("click", () => {
-            myGameListElement.innerHTML = "";
-            board_post.innerHTML = "글쓰기";
-            mygameList.innerHTML += `
-            <input type="text" placeholder="제목을 입력해 주세요."><p>
-            <div id="editor"></div>
-            `
-            createEditor();
-
-            const userPost = JSON.parse(localStorage.getItem('userPost')) || [];
-            userPost.push({})
-            localStorage.setItem("userPost", JSON.stringify(userPost));
+    //글쓰기 화면열기
+    document.querySelectorAll(".board_bt").forEach(bt => {
+        bt.addEventListener("click", () => {
+            writePost(category);
         })
     });
-});
-//  category, title, content, date 
+};
 
+//게시글 쓰기 폼
+const writePost = (category) => {
+
+    myGameListElement.innerHTML = "";
+    board_post.innerHTML = "글쓰기";
+    myGameListElement.innerHTML += `
+            <div>
+                <select id="boardCategory">
+                    <option value="전체게시판">전체게시판</option>
+                    <option value="자유게시판">자유게시판</option>
+                    <option value="공략/정보">공략/정보</option>
+                </select>
+                <input id="userTitle" type="text" placeholder="제목을 입력해 주세요.">
+                    <button id="cancelBt">취소</button>
+                    <button id="saveBt">등록</button>
+                    <div id="editor"></div>
+                
+            </div>
+            `
+    createEditor();
+
+    const boardCancel = document.querySelector("#cancelBt");
+    const boardSave = document.querySelector("#saveBt");
+
+    //글쓰기 취소
+    boardCancel.addEventListener("click", () => {
+        showboardList(category);
+    });
+    //글쓰기 세이브
+    boardSave.addEventListener("click", () => {
+        const userCategory = document.querySelector("#boardCategory").value;
+        const userTitle = document.querySelector("#userTitle").value;
+        const Content = editor.getHTML();
+        const date = new Date().toISOString();
+        const userPost = JSON.parse(localStorage.getItem('userPost')) || [];
+        //보안을 위한 정규식 이건 많이사용될듯..
+        const userContent = Content.replace(/<[^>]*>?/gm, '').trim();
+
+        //글쓰기 데이터 저장
+        if (userTitle.length < 2) {
+            alert("제목은 2글자 이상이어야 합니다.");
+            return;
+
+        } if (userContent.length === 0) {
+            alert("내용은 비워둘 수 없습니다.");
+            return;
+        } else {
+            userPost.push({ userCategory, userTitle, userContent, date })
+            localStorage.setItem("userPost", JSON.stringify(userPost));
+
+            alert("작성 완료!");
+            showboardList(category);
+        }
+    });
+};
+
+document.querySelectorAll(".board a").forEach(board => {
+    board.addEventListener("click", () => {
+        showboardList(board.innerText);
+    });
+});
+console.log(getUserPost());
